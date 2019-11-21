2cf301c0911704b49fe0a450e16616f732663729
张彬彬 <zhangbinbin@qutoutiao.net>
2019-11-21 10:40:18 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import UiCom from "./ui_component"
 import Item from "./item"
 // import store from './redux/store'
 import { getDeleteAction, getAddAction, getInputChangeAction, getTodoList } from './redux/actionCreators'
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   constructor(props) {
@@ -17,7 +18,17 @@ class App extends Component {
 
   render() {
     return (
-      <UiCom value={this.props.inputValue} handleChange={this.props.inputChange.bind(this)} handleClick={this.props.submit.bind(this)} itemArr={this.props.itemArr.bind(this)}></UiCom>
+      <Router>
+        <Fragment>
+          <UiCom value={this.props.inputValue} handleChange={this.props.inputChange.bind(this)} handleClick={this.props.submit.bind(this)} itemArr={this.props.itemArr.bind(this)}></UiCom>
+          <ul>
+            <li><Link to='/home'>home</Link></li>
+            <li><Link to='detail'>detail</Link></li>
+          </ul>
+          <Route path='/home' exact render={() => (<div>我是首页</div>)}></Route>
+          <Route path='/detail' exact render={() => (<div>我是详情</div>)}></Route>
+        </Fragment>
+      </Router>
     )
   }
 }
