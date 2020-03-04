c72009fded053d27cca1bbbb1e5fb4abeaa71077
zhangbinbin <zhangbinbin@qutoutiao.net>
2020-03-04 16:32:54 +0800

    feat:

--- src/App.js
+++ src/App.js
@@ -1,142 +1,43 @@
-import React, { Component, Fragment } from "react"
-import Item from "./item"
-import Axios from "axios"
-import './App.css'
-import store from "./redux/store"
+import React, { useState, useEffect } from 'react'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.removeItem = this.removeItem.bind(this)
-    this.handleAnima = this.handleAnima.bind(this)
-    this.handleStoreChange = this.handleStoreChange.bind(this)
-    this.state = store.getState()
-    store.subscribe(this.handleStoreChange)
-  }
-  componentWillMount() {
-    console.log('componentWillMount')
-  }
-  submit() {
-    // let [...list] = this.state.list
-    // list.push(this.state.value)
-    // this.setState({
-    //   list,
-    //   value: ""
-    // })
-
-    const action = {
-      type: "add_item",
+export default () => {
+  const [count, setCount] = useState(0)
+  const [isShow, setShowCom] = useState(true)
+  console.log('render')
+  useEffect(() => {
+    console.log('effect 执行')
+    return () => {
+      console.log('componentWillUnmount')
     }
-    store.dispatch(action)
-  }
-  inputChange(e) {
-    // let value = e.target.value
-    let value = this.input.value
-    // this.setState(() => ({
-    //   value
-    // }), () => {
-    //   console.log(this.state.value, 2)
-    // })
-    // console.log(this.state.value, 1)
-
-    const action = {
-      type: "input_change",
-      value
-    }
-    store.dispatch(action)
-  }
-  handleStoreChange() {
-    this.setState(() => (store.getState()))
-  }
-  removeItem(index) {
-    // let list = [...this.state.list]
-    // list.splice(index, 1)
-    // this.setState((prevState) => {
-    //   console.log(prevState === this.state)
-    //   return {
-    //     list
-    //   }
-    // })
-    const action = {
-      type: "delete_item",
-      index
-    }
-    store.dispatch(action)
-  }
-  itemArr() {
-    return this.state.list.map((item, index) => {
-      return (
-        <Item itemData={item} deleteItem={this.removeItem} index={index} key={index}/>
-      )
-    })
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount')
-    Axios.get("/api/test")
-    .then(res => {
-      console.log(res)
-      this.setState(() => (
+  }, [])
+  useEffect(() => {
+    console.log('componentDidUpdate')
+  }, [count])
+  return (
+    <div>
+      { count }
+      <button onClick={() => { setCount(x => x+1); setShowCom(x => !x) }}>+1</button>
+      <div>
         {
-          list: [...res.data]
+          isShow
+          ?
+          <Item></Item>
+          :
+          ""
         }
-      ))
-    })
-    .catch(err => {
-      console.log(err, "在这")
-    })
-  }
-  
-  shouldComponentUpdate() {
-    console.log('shouldComponentUpdate')
-    return true
-  }
-  
-  componentWillUpdate() {
-    console.log('componentWillUpdate')
-  }
-
-  // eslint-disable-next-line no-dupe-class-members
-  componentWillUpdate() {
-    console.log('componentWillUpdate')
-  }
-  
-  componentDidUpdate() {
-    console.log('componentDidupdate')
-  }
-  componentWillUnmount() {
-    console.log('componentWillUnmount')
-  }
-
-  handleAnima() {
-    console.log('执行了')
-    // this.setState(() => ({
-    //   animate: !this.state.animate
-    // }))
-  }
-
-  render() {
-    console.log("father render")
-    return (
-      <Fragment>
-        <input type="text" 
-          value={this.state.inputValue} 
-          onChange={this.inputChange.bind(this)}
-          ref={(input) => {
-            this.input = input
-          }}
-        /> 
-          <button onClick={this.submit.bind(this)}>提交</button>
-        <ul>
-          {
-            this.itemArr()
-          }
-        </ul>
-        <button onClick={this.handleAnima}>点击动画</button>
-        <div className={'amia'}>11111</div>
-      </Fragment>
-    )
-  }
+      </div>
+    </div>
+  )
 }
 
-export default App
+function Item () {
+  const [itemState, setItemSt] = useState(0)
+  useEffect(() => {
+    return () => {
+      console.log('item componentWillUnmount')
+    }
+  }, [itemState])
+  return (
+    <div>item 组件</div>
+  )
+}
